e96383fa3ab2466287c69811984258dedd1ea83c
Thialy Lima <thialymedeiros@gmail.com>
2024-05-06 16:07:08 -0300

    Funções para funcionamento do carregamento por comando

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// função para ativar popup
 function toggle(popupID){
 
     var popup = document.getElementById(popupID);
@@ -9,3 +10,29 @@ function toggle(popupID){
 }
 
 
+// Função para carregar mais cards
+function loadMore() {
+    const cards = document.querySelectorAll('.cards-box.hidden');
+
+    const batchSize = 8; // Número de cards a serem mostrados em cada lote
+  const endIndex = Math.min(cards.length, batchSize); // Calcula o índice final do lote
+  for (let i = 0; i < endIndex; i++) {
+    const card = cards[i];
+    // Adiciona a classe para mostrar o card
+    card.classList.remove('hidden');
+  }
+
+    // Esconde o botão "Carregar mais" se todos os cards estiverem visíveis
+    if (document.querySelectorAll('.cards-box.hidden').length === 0) {
+      document.getElementById('load-more').style.display = 'none';
+    }
+  }
+  
+  // Inicialização: esconder todos os cards exceto os primeiros 4
+  document.addEventListener('DOMContentLoaded', function() {
+    const cards = document.querySelectorAll('.cards-box');
+    for (let i = 8; i < cards.length; i++) {
+      cards[i].classList.add('hidden');
+    }
+  });
+  
